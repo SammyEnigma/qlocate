bb7d86d5a562d57da45c0d7a5d7b0f9ea1bb9dc1
Alexander Almaleh <sashoalm@gmail.com>
2017-11-26 12:51:41 +0200

    Document some of the functions.
    They are private slots actually but still good to document them.

--- mainwindow.h
+++ mainwindow.h
@@ -24,22 +24,55 @@ public:
     ~MainWindow();
 
 private slots:
+    /// Start a search.
     void startSearching();
+
+    /// Stop a search if one is ongoing.
     void stopSearching();
+
+    /// Used by the system tray.
     void toggleVisible(QSystemTrayIcon::ActivationReason);
+
+    /// Hides the window if focused, or shows it if hidden. If it was visible
+    /// but not focused, it "jumps" to the foreground. This is the most expected
+    /// behavior from similar apps.
     void toggleVisible();
+
+    /// Read new lines from locate's stdout if available.
     void readLocateOutput();
+
+    /// Show an error message in the status bar if locate process fails.
     void locateProcessError();
+
+    /// Quit the application. Note that closing the window closes it to the tray.
     void quit();
+
+    /// Open a file from the list widget (same as if you double-clicked it in
+    /// Explorer/a file manager).
     void openFile();
+
+    /// Show a file in its current folder.
     void showFile();
+
+    /// Start the updatedb tool. Might use gksu on Ubuntu.
     void startUpdateDB();
+
+    /// Show the context menu (on right click in the list widget)
     void showContextMenu(QPoint p);
+
+    /// Animate the ellipsis that acts as our "Searching..." animation.
     void animateEllipsis();
+
+    /// When full paths is on, the full paths are shown in the list widget - i.e.
+    /// /path/to/file.txt instead of file.txt.
     void toggleFullPaths();
+
 #ifndef DISABLE_QXT
+    /// Shows a dialog to change the global hotkey for showing the qlocate window.
     void changeGlobalHotkey();
 #endif
+
+    /// Restores the settings to their defaults.
     void restoreDefaults();
 
 private:
